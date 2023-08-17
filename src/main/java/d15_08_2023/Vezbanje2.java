635d5e4e2d6d3206891a723e8524b04eb7f5b571
RadovanovicMilica <mickebgbd5@gmail.com>
2023-08-17 12:17:30 +0200

    Add file download time estimation program

---
+++ src/main/java/d15_08_2023/Vezbanje2.java
@@ -0,0 +1,18 @@
+package d15_08_2023;
+
+public class Vezbanje2 {
+
+    public static void main(String[] args) {
+
+        String fileName= "Profile-image.png";
+        int originalSize= 10;
+        int downloadSize= 3;
+
+        System.out.println(fileName + " " + originalSize + "Mb");
+        System.out.println(downloadSize + "Mb");
+        System.out.println("****************************");
+        System.out.println("******************30%*********");
+        System.out.println("*******************************");
+
+    }
+}
