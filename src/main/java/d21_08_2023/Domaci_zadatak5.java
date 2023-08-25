a931526f1c02f1163e0766e0ff92f116136176b1
RadovanovicMilica <mickebgbd5@gmail.com>
2023-08-25 09:06:28 +0200

    Modified homework 21.08.2023. task 5

--- src/main/java/d21_08_2023/Domaci_zadatak5.java
+++ src/main/java/d21_08_2023/Domaci_zadatak5.java
@@ -1,5 +1,8 @@
 package d21_08_2023;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Domaci_zadatak5 {
     public static void main(String[] args) {
 
@@ -8,9 +11,32 @@ public class Domaci_zadatak5 {
 //        dva random broja koja treba da sabere i poruka o tacnosti resenja.
 //        (Program generise random brojeve u opsegu od 0 do 50, ne unosi ih korisnik)
 
+        Scanner s = new Scanner(System.in);
+
+        int tacanOdgovor = 0;
+        int brojTacnihOdgovora = 0;
+        Random random = new Random();
+
+        for (int i = 1; i <= 5; i++) {
+            int x = random.nextInt(50);
+            int y = random.nextInt(50);
+
+            tacanOdgovor = x + y;
+            System.out.println("Koliko je " + x + "+" + y + "?");
+
+            int realniOdgovor = s.nextInt();
+
+            if (tacanOdgovor == realniOdgovor) {
+                System.out.println("Cestitamo!");
+                brojTacnihOdgovora++;
+            } else if (tacanOdgovor != realniOdgovor) {
+                System.out.println("Greska!");
+            }
+
+        } // zatvaram petlju
 
 
-
+        System.out.println("Broj tacnih odgovora je: " + brojTacnihOdgovora + "/5");
     } // zatvaram metodu
 
 } //zatvaram klasu
